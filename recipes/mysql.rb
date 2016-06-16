671b410252a791a6f2eaf37e3f43aa1a8bda259a
Masashi Terui <masashi.terui@jig-saw.com>
2016-06-16 19:36:29 +0900

    save

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -7,6 +7,8 @@
 # Apache 2.0 License
 #
 
+include_recipe 'database::mysql'
+
 if platform_family?('rhel')
   pkg = "mysql#{node['oreno']['mysql_version'].sub('.', '')}-community-release-el#{node['platform_version'].to_i}-8.noarch.rpm"
 end
@@ -23,8 +25,11 @@ remote_file "#{node['oreno']['base_dir']}/#{pkg}" do
   notifies :install, 'rpm_package[mysql_repo]', :immediately
 end
 
+package 'gzip'
 package 'mysql-community-server'
-package 'mysql-utilities'
+package 'mysql-community-libs'
+package 'mysql-community-utilities'
+package 'mysql-community-client'
 
 directory '/etc/mysql.d'
 
@@ -36,8 +41,9 @@ template '/etc/my.cnf' do
    :report_host => node['ipaddress'],
    :datadir => node['oreno_rds']['datadir'],
    :socket => node['oreno_rds']['socket'],
-   :log_error => node['oreno_rds']['log-error'],
-   :pid_file => node['oreno_rds']['pid-file'],
+   :log_error => node['oreno_rds']['log_error'],
+   :pid_file => node['oreno_rds']['pid_file'],
+   :server_type => node['oreno_rds']['server_type'],
   })
 end
 
@@ -45,3 +51,33 @@ service 'mysqld' do
   supports :status => true, :restart => true, :reload => false
   action [:enable, :start]
 end
+
+mysql_connection_info = {:host => 'localhost', :username => 'root'}
+
+mysql_database_user node['oreno']['replica_user'] do
+  connection mysql_connection_info
+  host node['oreno']['replica_hosts']
+  password node['oreno']['replica_password']
+  privileges ['REPLICATION SLAVE']
+  action [:create, :grant]
+end
+
+file "#{node['oreno']['base_dir']}/full-backup.sh" do
+  mode '0755'
+  content <<-EOL
+TODAY = `date +"%Y%m%d"`
+mysqldump -u root --all-databases --single-transaction --master-data 2 | gzip > /tmp/$TODAY.gz
+aws s3 put-object /tmp/$TODAY.gz s3://#{node['oreno']['backup_bucket']}/full/$TODAY.gz
+rm -f /tmp/$TODAY.gz
+EOL
+  action :create
+end
+
+cron 'full-backup' do
+  command "/bin/sh #{node['oreno']['base_dir']}/full-backup.sh"
+  hour node['oreno']['daily_backup_hour']
+  minute node['oreno']['daily_backup_minutes']
+  only_if node['oreno']['server_type'] == 'standby'
+end
+
+# TODO: PITR
